8d818ec9d001fd5a3b70aa9d37be09a040b1d917
Qing Zheng <zhengq@cs.cmu.edu>
2016-06-22 19:13:55 -0400

    Fix output format

--- src/gigaplus_test.cc
+++ src/gigaplus_test.cc
@@ -475,7 +475,7 @@ TEST(DirIndexTest, Migration4) {
 static void PrintStates(const std::vector<int>& states) {
   static int run = 0;
   fprintf(stderr, "case %02d: ", ++run);
-  fprintf(stderr, "num_server: %d\t", int(states.size()));
+  fprintf(stderr, "num_server: %4d\t", int(states.size()));
   int max = *std::max_element(states.begin(), states.end());
   fprintf(stderr, "max: %d\t", max);
   int min = *std::min_element(states.begin(), states.end());
