1db9d2adcd88f2d299642e7a01318f66cf7f36b2
Qing Zheng <zhengq@cs.cmu.edu>
2016-09-19 14:46:38 -0400

    Add two-level-bucketing ect

--- src/ectindex.cc
+++ src/ectindex.cc
@@ -58,17 +58,141 @@ class EntropyCodedTrie : public ECT {
   }
 };
 
+class TwoLevelBucketingTrie : public ECT {
+  static const size_t kSkipBits = 0;
+  static const size_t kDestBase = 0;
+  static const size_t kDestKeysPerBlock = 1;
+
+  struct Builder {
+    std::vector<const uint8_t*> pending_keys_;
+    size_t last_dest_offset_;
+    size_t bucket_count_;
+    size_t pending_key_count_;
+  };
+
+ private:
+  typedef ectrie::bit_vector<> bitvec_t;
+  bitvec_t bitvector_;
+  typedef ectrie::trie<> trie_t;
+  trie_t trie_;
+  typedef ectrie::twolevel_bucketing<> bucket_t;
+  bucket_t bucketing_;
+  size_t key_len_;
+  size_t n_;
+
+  // Initialized lazily
+  size_t bucket_bits_;
+  size_t bucket_size_;
+  size_t bucket_count_;
+
+ public:
+  TwoLevelBucketingTrie(size_t key_len) : key_len_(key_len), n_(0) {}
+  virtual ~TwoLevelBucketingTrie() {}
+
+  virtual size_t MemUsage() const {
+    return bitvector_.size() + bucketing_.bit_size();
+  }
+
+  virtual size_t Locate(const uint8_t* key) const {
+    size_t b = FindBucket(key);
+
+    size_t iter = bucketing_.index_offset(b);
+    size_t off = bucketing_.dest_offset(b);
+    size_t n = bucketing_.dest_offset(b + 1) - off;
+
+    size_t index =
+        off +
+        trie_.locate(bitvector_, iter, key, key_len_, 0, n, kDestBase + off,
+                     kDestKeysPerBlock, kSkipBits + bucket_bits_);
+
+    return index;
+  }
+
+  virtual size_t Find(const Slice& key) const {
+    return Locate(reinterpret_cast<const uint8_t*>(key.data()));
+  }
+
+  void InsertKeys(size_t n, const uint8_t** keys) {
+    n_ = n;
+    size_t bucket_size = 256;
+    bucket_bits_ = 0;
+    while ((static_cast<size_t>(1) << bucket_bits_) < (n_ / bucket_size)) {
+      bucket_bits_++;
+    }
+    bucket_count_ = static_cast<size_t>(1) << bucket_bits_;
+    bucket_size_ = n_ / bucket_count_;
+    if (bucket_size_ == 0) {
+      bucket_size_ = 1;
+    }
+
+    bucketing_.resize(bucket_count_ + 1, bucket_size_, kDestKeysPerBlock);
+    bucketing_.insert(0, 0);
+
+    Builder bu;
+    bu.last_dest_offset_ = 0;
+    bu.bucket_count_ = 0;
+    bu.pending_key_count_ = 0;
+    for (size_t i = 0; i < n_; i++) AppendKey(keys[i], bu);
+    while (bu.bucket_count_ < bucket_count_) {
+      Flush(bu);
+    }
+
+    bucketing_.finalize();
+  }
+
+ private:
+  void AppendKey(const uint8_t* key, Builder& bu) {
+    assert(bu.bucket_count_ < bucket_count_);
+    size_t b = FindBucket(key);
+    assert(b >= bu.bucket_count_);
+    while (bu.bucket_count_ < b) {
+      Flush(bu);
+    }
+
+    bu.pending_key_count_++;
+    bu.pending_keys_.push_back(key);
+  }
+
+  void Flush(Builder& bu) {
+    assert(bu.bucket_count_ < bucket_count_);
+    trie_.encode(bitvector_, bu.pending_keys_, key_len_, 0,
+                 bu.pending_key_count_, kDestBase + bu.last_dest_offset_,
+                 kDestKeysPerBlock, kSkipBits + bucket_bits_);
+
+    bu.bucket_count_ += 1;
+    bucketing_.insert(bitvector_.size(),
+                      bu.last_dest_offset_ + bu.pending_key_count_);
+
+    bu.last_dest_offset_ += bu.pending_key_count_;
+    bu.pending_keys_.clear();
+    bu.pending_key_count_ = 0;
+  }
+
+  size_t FindBucket(const uint8_t* key) const {
+    size_t bits = 0;
+    size_t index = 0;
+    while (bits < bucket_bits_) {
+      index <<= 1;
+      if (ectrie::bit_access::get(key, kSkipBits + bits)) {
+        index |= 1;
+      }
+      bits++;
+    }
+    return index;
+  }
+};
+
 }  // anonymous namespace
 
 ECT* ECT::Create(size_t key_len, size_t n, const Slice* keys) {
-  EntropyCodedTrie* ect = new EntropyCodedTrie(key_len);
+  TwoLevelBucketingTrie* trie = new TwoLevelBucketingTrie(key_len);
   std::vector<const uint8_t*> ikeys;
   ikeys.reserve(n);
   for (size_t i = 0; i < n; i++) {
     ikeys.push_back(reinterpret_cast<const uint8_t*>(keys[i].data()));
   }
-  ect->InsertKeys(ikeys.size(), ikeys.data());
-  return ect;
+  trie->InsertKeys(ikeys.size(), ikeys.data());
+  return trie;
 }
 
 }  // namespace pdlfs
