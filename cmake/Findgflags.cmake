9209d5a98438cb9083102f804a064d1c1d743a5d
Chuck Cranor <chuck@ece.cmu.edu>
2016-10-07 23:21:39 -0400

    make gflags target all lowercase to make it work with config and modules

--- cmake/Findgflags.cmake
+++ cmake/Findgflags.cmake
@@ -9,7 +9,7 @@
 #   - GFLAGS_LIBRARY_DIR: hint for finding gflags lib
 #
 # output:
-#   - "gflags" library target 
+#   - "gflags" library target
 #   - GFLAGS_FOUND  (set if found)
 #
 
@@ -18,7 +18,7 @@ include (FindPackageHandleStandardArgs)
 find_path (GFLAGS_INCLUDE gflags/gflags.h HINTS ${GFLAGS_INCLUDE_DIR})
 find_library (GFLAGS_LIBRARY gflags HINTS ${GFLAGS_LIBRARY_DIR})
 
-find_package_handle_standard_args (GFlags DEFAULT_MSG 
+find_package_handle_standard_args (gflags DEFAULT_MSG
     GFLAGS_INCLUDE GFLAGS_LIBRARY)
 
 mark_as_advanced (GFLAGS_INCLUDE GFLAGS_LIBRARY)
