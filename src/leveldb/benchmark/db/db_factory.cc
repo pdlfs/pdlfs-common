2e9bf9ce88fe76f312e8d155e4f47d6baa6fa978
LuZhang-Lou <lzhang3@andrew.cmu.edu>
2016-11-18 12:02:55 -0800

    import ycsb-c

---
+++ src/leveldb/benchmark/db/db_factory.cc
@@ -0,0 +1,26 @@
+//
+//  basic_db.cc
+//  YCSB-C
+//
+//  Created by Jinglei Ren on 12/17/14.
+//  Copyright (c) 2014 Jinglei Ren <jinglei@ren.systems>.
+//
+
+#include "db_factory.h"
+
+#include <string>
+#include "basic_db.h"
+#include "lock_stl_db.h"
+
+using namespace std;
+using ycsbc::DB;
+using ycsbc::DBFactory;
+
+DB* DBFactory::CreateDB(utils::Properties &props) {
+  if (props["dbname"] == "basic") {
+    return new BasicDB;
+  } else if (props["dbname"] == "lock_stl") {
+    return new LockStlDB;
+  } else return NULL;
+}
+
