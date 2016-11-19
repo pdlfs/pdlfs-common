babae0f08b218a16e42c36f0c412f9ef5fad6056
LuZhang-Lou <lzhang3@andrew.cmu.edu>
2016-11-19 12:57:21 -0800

    add expr output

--- src/leveldb/benchmark/ycsbc.cc
+++ src/leveldb/benchmark/ycsbc.cc
@@ -73,7 +73,7 @@ int main(const int argc, const char* argv[]) {
   }
   double duration = load_timer.End();
   cerr << "# Loading records:\t" << sum << endl;
-  cerr << "# Transaction throughput (KTPS)" << endl;
+  cerr << "# Loading throughput (KTPS)" << endl;
   cerr << props["dbname"] << '\t' << file_name << '\t' << num_threads << '\t';
   cerr << total_ops / duration / 1000 << endl;
 
@@ -94,6 +94,8 @@ int main(const int argc, const char* argv[]) {
     sum += n.get();
   }
   duration = timer.End();
+
+  cerr << "# Transaction records:\t" << sum << endl;
   cerr << "# Transaction throughput (KTPS)" << endl;
   cerr << props["dbname"] << '\t' << file_name << '\t' << num_threads << '\t';
   cerr << total_ops / duration / 1000 << endl;
