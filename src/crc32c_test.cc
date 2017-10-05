142fa70742279ab12256d2d1c80abcb16a239d68
Qing Zheng <zhengq@cs.cmu.edu>
2017-10-05 13:47:38 -0400

    update crc32c sse42 acceleration

--- src/crc32c_test.cc
+++ src/crc32c_test.cc
@@ -11,19 +11,24 @@
 #include "pdlfs-common/crc32c.h"
 #include "pdlfs-common/testharness.h"
 
+#include "crc32c_internal.h"
+
 namespace pdlfs {
 namespace crc32c {
 
-class CRC {};
+class CRC {
+ public:
+  CRC() : hw_(CanAccelerateCrc32c()) {}
+  int hw_;
+};
 
-extern int crc32c_has_sse42();
-
-TEST(CRC, SSE42) {
-  if (crc32c_has_sse42()) {
-    fprintf(stderr, "crc32c will use intel sse4.2\n");
+TEST(CRC, HW) {
+  if (hw_) {
+    fprintf(stderr, "crc32c hardware acceleration is available");
   } else {
-    fprintf(stderr, "intel sse4.2 not present\n");
+    fprintf(stderr, "crc32c hardware acceleration is off");
   }
+  fprintf(stderr, "\n");
 }
 
 TEST(CRC, StandardResults) {
@@ -31,20 +36,24 @@ TEST(CRC, StandardResults) {
   char buf[32];
 
   memset(buf, 0, sizeof(buf));
-  ASSERT_EQ(0x8a9136aa, Value(buf, sizeof(buf)));
+  ASSERT_EQ(0x8a9136aa, ValueSW(buf, sizeof(buf)));
+  if (hw_) ASSERT_EQ(0x8a9136aa, ValueHW(buf, sizeof(buf)));
 
   memset(buf, 0xff, sizeof(buf));
-  ASSERT_EQ(0x62a8ab43, Value(buf, sizeof(buf)));
+  ASSERT_EQ(0x62a8ab43, ValueSW(buf, sizeof(buf)));
+  if (hw_) ASSERT_EQ(0x62a8ab43, ValueHW(buf, sizeof(buf)));
 
   for (int i = 0; i < 32; i++) {
     buf[i] = static_cast<char>(i);
   }
-  ASSERT_EQ(0x46dd794e, Value(buf, sizeof(buf)));
+  ASSERT_EQ(0x46dd794e, ValueSW(buf, sizeof(buf)));
+  if (hw_) ASSERT_EQ(0x46dd794e, ValueHW(buf, sizeof(buf)));
 
   for (int i = 0; i < 32; i++) {
     buf[i] = static_cast<char>(31 - i);
   }
-  ASSERT_EQ(0x113fdb5c, Value(buf, sizeof(buf)));
+  ASSERT_EQ(0x113fdb5c, ValueSW(buf, sizeof(buf)));
+  if (hw_) ASSERT_EQ(0x113fdb5c, ValueHW(buf, sizeof(buf)));
 
   unsigned char data[48] = {
       0x01, 0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -52,49 +61,22 @@ TEST(CRC, StandardResults) {
       0x00, 0x00, 0x00, 0x14, 0x00, 0x00, 0x00, 0x18, 0x28, 0x00, 0x00, 0x00,
       0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
   };
-  ASSERT_EQ(0xd9963a56, Value(reinterpret_cast<char*>(data), sizeof(data)));
+  ASSERT_EQ(0xd9963a56, ValueSW(reinterpret_cast<char*>(data), sizeof(data)));
+  if (hw_)
+    ASSERT_EQ(0xd9963a56, ValueHW(reinterpret_cast<char*>(data), sizeof(data)));
 }
 
-TEST(CRC, StandardResultsHW) {
-  // From rfc3720 section B.4.
-  char buf[32];
-
-  memset(buf, 0, sizeof(buf));
-  ASSERT_EQ(0x8a9136aa, ValueHW(buf, sizeof(buf)));
-
-  memset(buf, 0xff, sizeof(buf));
-  ASSERT_EQ(0x62a8ab43, ValueHW(buf, sizeof(buf)));
-
-  for (int i = 0; i < 32; i++) {
-    buf[i] = static_cast<char>(i);
-  }
-  ASSERT_EQ(0x46dd794e, ValueHW(buf, sizeof(buf)));
-
-  for (int i = 0; i < 32; i++) {
-    buf[i] = static_cast<char>(31 - i);
-  }
-  ASSERT_EQ(0x113fdb5c, ValueHW(buf, sizeof(buf)));
-
-  unsigned char data[48] = {
-      0x01, 0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-      0x00, 0x00, 0x00, 0x00, 0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x00,
-      0x00, 0x00, 0x00, 0x14, 0x00, 0x00, 0x00, 0x18, 0x28, 0x00, 0x00, 0x00,
-      0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-  };
-  ASSERT_EQ(0xd9963a56, ValueHW(reinterpret_cast<char*>(data), sizeof(data)));
+TEST(CRC, Values) {
+  ASSERT_NE(ValueSW("a", 1), ValueSW("foo", 3));
+  if (hw_) ASSERT_NE(ValueHW("a", 1), ValueHW("foo", 3));
 }
-
-TEST(CRC, Values) { ASSERT_NE(Value("a", 1), Value("foo", 3)); }
-
-TEST(CRC, ValuesHW) { ASSERT_NE(ValueHW("a", 1), ValueHW("foo", 3)); }
 
 TEST(CRC, Extend) {
-  ASSERT_EQ(Value("hello world", 11), Extend(Value("hello ", 6), "world", 5));
-}
-
-TEST(CRC, ExtendHW) {
-  ASSERT_EQ(ValueHW("hello world", 11),
-            ExtendHW(ValueHW("hello ", 6), "world", 5));
+  ASSERT_EQ(ValueSW("hello world", 11),
+            ExtendSW(ValueSW("hello ", 6), "world", 5));
+  if (hw_)
+    ASSERT_EQ(ValueHW("hello world", 11),
+              ExtendHW(ValueHW("hello ", 6), "world", 5));
 }
 
 TEST(CRC, Mask) {
